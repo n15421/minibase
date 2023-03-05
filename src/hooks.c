51ae06c769c07a6d22109a0fc3bf5085f7e04947
WillowSauceR <WillowSauceR@outlook.com>
2023-03-05 17:38:56 +0800

    todo: Add TMCALL macro

--- src/hooks.c
+++ src/hooks.c
@@ -1,13 +1,31 @@
 #include <hooker/hooks.h>
 
+const enum log_level
+{
+    INFO = 2u,
+    WARN = 4u,
+    UNKNOWN = 6u,
+    ERR = 8u
+};
+
 bool server_started = false;
 TMHOOK(on_server_started, void,
         "?startServerThread@ServerInstance@@QEAAXXZ",
         uintptr_t _this)
 {
     server_started = true;
+    
+    // TODO: Add SYMCALL macro
+    ((char (*)(unsigned int a1, char a2, int a3, int a4, unsigned int a5, const char *a6, int a7, const char *a8))
+        get_sym_func("?log@BedrockLog@@YAXW4LogCategory@1@V?$bitset@$02@std@@W4LogRule@1@W4LogAreaID@@IPEBDH4ZZ"))
+        (0, 1, 0, 12, UNKNOWN, "HOOKER->LOG", 114514, "injected!");
+    
+    char (*bedrock_log_log)(unsigned int a1, char a2, int a3, int a4, unsigned int a5, const char *a6, int a7, const char *a8) = 
+        get_sym_func("?log@BedrockLog@@YAXW4LogCategory@1@V?$bitset@$02@std@@W4LogRule@1@W4LogAreaID@@IPEBDH4ZZ");
+    bedrock_log_log(0, 1, 0, 12, WARN, "HOOKER->LOG", 1919810, "feel free!");
+
     on_server_started.original(_this);
-}                               
+}
 
 TMHOOK(on_console_output, bool, 
         "??$_Insert_string@DU?$char_traits@D@std@@_K@std@@YAAEAV?$basic_ostream@DU?$char_traits@D@std@@@0@AEAV10@QEBD_K@Z",
