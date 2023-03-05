6883d1270cfe27c359fc2f101fb61ee8d7426ea7
WillowSauceR <WillowSauceR@outlook.com>
2023-03-05 14:14:33 +0800

    refactor: hide unused func/struct/type

--- include/hook_api/hook.h
+++ include/hook_api/hook.h
@@ -11,37 +11,37 @@
 #define CVDUMP_URL "https://raw.github.com/microsoft/microsoft-pdb/master/cvdump/cvdump.exe"
 
 #define TMHOOK(name, ret_type, rva_OR_sym, ...)             \
-    typedef ret_type (*name##_t)(__VA_ARGS__);              \
-    name##_t original_##name = NULL;                        \
-    typedef struct _##name name##_struct;                   \
+    typedef ret_type (*_##name##_t)(__VA_ARGS__);           \
+    _##name##_t _original_##name = NULL;                    \
+    typedef struct _##name _##name##_struct;                \
     struct _##name                                          \
     {                                                       \
-        name##_t original;                                  \
-        name##_t detour;                                    \
-        bool (*init)(name##_struct*);                       \
+        _##name##_t original;                               \
+        _##name##_t detour;                                 \
+        bool (*init)(_##name##_struct*);                    \
     };                                                      \
-    ret_type detour_##name(__VA_ARGS__);                    \
-    bool _INIT_HOOK_##name(name##_struct* name)             \
+    ret_type _detour_##name(__VA_ARGS__);                   \
+    bool _INIT_HOOK_##name(_##name##_struct* name)          \
     {                                                       \
         void *func_ptr = atoi(rva_OR_sym)                   \
                         ? get_rva_func(atoi(rva_OR_sym))    \
                         : get_sym_func(rva_OR_sym);         \
-        name##_t hook_##name =                              \
-                        (name##_t)func_ptr;                 \
-        bool result = hook_func(hook_##name,                \
-                                detour_##name,              \
-                                &original_##name);          \
-        name->original = original_##name;                   \
-        name->detour = detour_##name;                       \
+        _##name##_t _hook_##name =                          \
+                        (_##name##_t)func_ptr;              \
+        bool result = hook_func(_hook_##name,               \
+                                _detour_##name,             \
+                                &_original_##name);         \
+        name->original = _original_##name;                  \
+        name->detour = _detour_##name;                      \
         return result;                                      \
     }                                                       \
-    name##_struct name =                                    \
+    _##name##_struct name =                                 \
     {                                                       \
         NULL,                                               \
         NULL,                                               \
         _INIT_HOOK_##name                                   \
     };                                                      \
-    ret_type detour_##name(__VA_ARGS__)
+    ret_type _detour_##name(__VA_ARGS__)
 
 #ifdef __cplusplus
 extern "C" {
