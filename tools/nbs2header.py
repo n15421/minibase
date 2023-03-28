2bc5bf7ffbeec3384bc896e6d0048e780153ad0d
student_2333 <lgc2333@126.com>
2023-03-29 02:03:37 +0800

    fix: change demical places

--- tools/nbs2header.py
+++ tools/nbs2header.py
@@ -56,7 +56,7 @@ def gen_header(processed: list[ProcessedNote]) -> str:
         "const float NOTE_DATA[MAX_NOTE_LEN][4] = {\n"
     )
     body = "".join(
-        [f"\t{{ {a}, {b}, {c}, {round(d, 5)} }},\n" for a, b, c, d in processed]
+        [f"\t{{ {a}, {b}, {c}, {round(d, 6)} }},\n" for a, b, c, d in processed]
     )
     tail = "};\n"
 
