476ba16192d64954fc74dee724c857c77ae7dea3
Syncyn-R <xiziya_r@outlook.com>
2024-06-03 08:20:38 +0800

    feat: uninstall hook supported

--- src/linux/hook.c
+++ src/linux/hook.c
@@ -6,7 +6,7 @@
 
 extern long long g_lb_base_addr;
 
-void *lb_ahook_install(void **in_target_addr, void *in_hook_addr)
+void *lb_ahook_install(void **in_target_addr, void *in_hook_addr, void **out_handle)
 {
 	int ret = 0;
 
@@ -28,15 +28,36 @@ void *lb_ahook_install(void **in_target_addr, void *in_hook_addr)
 		return NULL;
 	}
 
+	*out_handle = (void *)hook;
+
 	return *in_target_addr;
 }
 
-void *lb_shook_install(const char *in_sym, void *in_hook_addr)
+void *lb_shook_install(const char *in_sym, void *in_hook_addr, void **out_handle)
 {
 	void *addr = lb_sym_find(in_sym);
 	if (!addr) {
 		printf("[LightBase] [ERROR] Symbol not found: %s.\n", in_sym);
 	}
 
-	return lb_ahook_install(&addr, in_hook_addr);
+	return lb_ahook_install(&addr, in_hook_addr, out_handle);
+}
+
+void lb_hook_uninstall(const char *in_sym, void *in_handle)
+{
+	int ret = 0;
+
+	ret = funchook_uninstall((funchook_t *)in_handle, 0);
+	if (ret) {
+		printf("[LightBase] [ERROR] Hook uninstall failed: %s.\n", funchook_error_message((funchook_t *)in_handle));
+
+		return;
+	}
+
+	ret = funchook_destroy((funchook_t *)in_handle);
+	if (ret) {
+		printf("[LightBase] [ERROR] Hook destory failed: %s.\n", funchook_error_message((funchook_t *)in_handle));
+
+		return;
+	}
 }
