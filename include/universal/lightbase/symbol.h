d945a5f24388267251d0cf0e07a99556f8f3b580
Syncyn-R <xiziya_r@outlook.com>
2024-06-07 07:58:47 +0800

    update: fmt doc

--- include/universal/lightbase/symbol.h
+++ include/universal/lightbase/symbol.h
@@ -14,3 +14,9 @@ void lb_sym_free();
 #ifdef __cplusplus
 }
 #endif
+
+#define SCALL(sym, func_proto, ...) \
+	((func_proto)lb_sym_call_find(sym))(__VA_ARGS__)
+
+#define ACALL(addr, func_proto, ...) \
+	((func_proto)addr)(__VA_ARGS__)
